b1ea21fa4ea12adb67c4d8f6f5d499b7f77fa741
Sergey <samtriezo@gmail.com>
2023-05-23 20:34:52 +0700

    добавил комментарии к коду необязательной задачи 1, семинар 2

--- Семинар 2/Задача необязательная 1/Program.cs
+++ Семинар 2/Задача необязательная 1/Program.cs
@@ -1,9 +1,9 @@
 ﻿System.Console.Write("Введите целое или вещественное число: ");
 double num = Convert.ToDouble(System.Console.ReadLine());
 
-int Count = 0;
+int Count = 0; 
 double temp = num;
-while (temp > 9)
+while (temp > 9) //определяем сколько десятков в числе
 {
     temp = temp / 10;
     Count++;
@@ -13,6 +13,6 @@ if(Count == 0)
 else
 {
     temp = num;
-    Console.Write(Convert.ToInt32(temp) / Convert.ToInt32(Math.Pow(10, Count)));
-    if(Count > 1) Console.Write(temp % Math.Pow(10, Count - 1));
+    Console.Write(Convert.ToInt32(temp) / Convert.ToInt32(Math.Pow(10, Count))); //делим число на число в степени десятков, получаем первую цифру
+    if(Count > 1) Console.Write(temp % Math.Pow(10, Count - 1)); //получаем остаток от деления на число в степени десятков - 1, чтобы пропустить вторую цифру
 }
