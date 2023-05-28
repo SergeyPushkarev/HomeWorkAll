fb80be1494482f50d3196625afe57717b590ada5
Sergey <samtriezo@gmail.com>
2023-05-28 14:49:25 +0700

    Сделал задачу 23 семинара 3

---
+++ Семинар 3/Задача 23/Program.cs
@@ -0,0 +1,23 @@
+﻿void ReadArray(int[] ArrayCubes)
+{
+    for(int i=0;i<=ArrayCubes.Length-1;i++)
+    {
+        System.Console.WriteLine(ArrayCubes[i]);
+    }
+}
+
+int[] ReturnArrayCubes(int num)
+{
+    int[] ArrayCubes = new int[num];
+    for(int i=0;i<=num-1;i++)
+    {
+        ArrayCubes[i] = Convert.ToInt32(Math.Pow(i+1,3));
+    }
+    return ArrayCubes;
+}
+
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(System.Console.ReadLine());
+
+int[] ArrayCubes = ReturnArrayCubes(num);
+ReadArray(ArrayCubes);
