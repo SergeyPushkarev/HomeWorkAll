1b7dc7b8fb62c8e022b9997a0264e11c3872b261
Sergey <samtriezo@gmail.com>
2023-06-02 18:02:19 +0700

    Сделал задачу 25 4 семестра

---
+++ Семинар 4/Задача 25/Program.cs
@@ -0,0 +1,16 @@
+﻿int RaiseToDegree(int numA, int numB)
+{
+    int temp = numA;
+    for (int i=1; i < numB; i++)
+    {
+        numA *= temp;
+    }
+    return numA;
+}
+
+System.Console.Write("Введите число A: ");
+int numA = Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.WriteLine(RaiseToDegree(numA, Math.Abs(numB)));
