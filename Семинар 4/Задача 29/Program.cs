479f6ca251f6762e8c0786189d43f9cbb8811696
Sergey <samtriezo@gmail.com>
2023-06-02 18:31:16 +0700

    Сделал задачу 29 4 семестра

---
+++ Семинар 4/Задача 29/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] GenerateArray(int[] Array)
+{
+    for (int i=0; i < Array.Length; i++)
+    {
+        Array[i] = new Random().Next(95,100);
+        System.Console.Write(Array[i]);
+        if(i+1 < Array.Length) System.Console.Write(", ");
+    }
+    return Array;
+}
+
+int[] Array = new int[8];
+Array = GenerateArray(Array);
