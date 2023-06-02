b8ec7b49d684faa1e3c5390fbbc39f3f729363a5
Sergey <samtriezo@gmail.com>
2023-06-02 18:20:08 +0700

    Сделал задачу 27 4 семестра

---
+++ Семинар 4/Задача 27/Program.cs
@@ -0,0 +1,15 @@
+﻿int CalculateSumDigitsNumber(int num)
+{
+    int SumDigitsNumber = 0;
+    while (num != 0)
+    {
+        SumDigitsNumber += num % 10;
+        num /= 10;
+    }
+    return SumDigitsNumber;
+}
+
+System.Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.WriteLine(CalculateSumDigitsNumber(num));
