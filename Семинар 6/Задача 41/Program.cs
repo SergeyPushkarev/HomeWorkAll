65a7d1012fd8ec98b17859dd0fd6b1a2a476fdce
Sergey <samtriezo@gmail.com>
2023-06-06 19:11:44 +0700

    сделал задачу 41 семинара 6

---
+++ Семинар 6/Задача 41/Program.cs
@@ -0,0 +1,14 @@
+﻿int EnterAndCalculateGreatZero(int Count = 0)
+{
+    string StringNum = System.Console.ReadLine()!;
+    if(StringNum.Length != 0)
+    {
+        if(Convert.ToInt32(StringNum) > 0) Count++;
+        Count = EnterAndCalculateGreatZero(Count);
+    }
+    else return Count;
+    return Count;
+}
+
+System.Console.WriteLine("Введите нужное количество чисел, отделяя их с помощью ENTER. Чтобы завершить ввод, отправьть пустую строку.");
+System.Console.WriteLine($"Введено чисел больше нуля: {EnterAndCalculateGreatZero()}");
