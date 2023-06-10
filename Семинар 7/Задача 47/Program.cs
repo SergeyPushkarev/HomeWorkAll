6f27a8a6bb98b1cf58c2412b124f43bc59cf714d
Sergey <samtriezo@gmail.com>
2023-06-10 23:01:41 +0700

    сделал задачу 47 семинара 7

---
+++ Семинар 7/Задача 47/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+void RandomFillArray(double[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+            Array[i,j] = Math.Round(new Random().Next(-20,21) + new Random().NextDouble(),2);
+    }
+}
+
+void PrintArray(double[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+            System.Console.Write($"{Array[i,j],6} ");
+        System.Console.WriteLine();
+    }
+}
+
+System.Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(System.Console.ReadLine());
+double[,] Array = new double[rows,columns];
+
+RandomFillArray(Array);
+PrintArray(Array);
