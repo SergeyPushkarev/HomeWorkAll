29f760c1f0761d787e5b3e001cab878c7d9d9d5e
Sergey <samtriezo@gmail.com>
2023-06-13 18:22:01 +0700

    сделал задачи 54, 56 семинара 8

---
+++ Семинар 8/Задача 54/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+void SortArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++) //сначала цикл по всем строкам массива
+    {
+        for (int j = 0; j < Array.GetLength(1); j++) //потом цикл по всем столбцам массива
+        {
+            int maxPosition = j; //выделим переменную, в которой будем хранить позицию самого максимального значения по строке
+            for (int k = j; k < Array.GetLength(1); k++) //запустим цикл снова по столбцам, но уже исходя из столбца, до которого дошли
+            {
+                if(Array[i,maxPosition] < Array[i,k]) //запоминаем позицию максимального значения
+                    maxPosition = k;
+            }
+            (Array[i,j], Array[i,maxPosition]) = (Array[i,maxPosition], Array[i,j]); //тут меняем местами максимальное значение и текущее
+        }
+    }
+}
+
+void RandomFillArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+            Array[i,j] = new Random().Next(1,21);
+    }
+}
+
+void PrintArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+            System.Console.Write($"{Array[i,j],3} ");
+        System.Console.WriteLine();
+    }
+}
+
+System.Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(System.Console.ReadLine());
+int[,] Array = new int[rows,columns];
+
+RandomFillArray(Array);
+PrintArray(Array);
+System.Console.WriteLine();
+SortArray(Array);
+PrintArray(Array);
---
+++ Семинар 8/Задача 56/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+void FindLargestSumOfElements(int[,] Array)
+{
+    int[] maxSum = new int[2];
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        int Sum = 0;
+        for (int j = 0; j < Array.GetLength(1); j++)
+            Sum += Array[i,j];
+        if(maxSum[0] < Sum)
+        {
+            maxSum[0] = Sum;
+            maxSum[1] = i;
+        }
+    }
+    System.Console.WriteLine($"Максимальная сумма элементов в строке {maxSum[1]+1}, сумма равняется {maxSum[0]}");
+}
+
+void RandomFillArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+        for (int j = 0; j < Array.GetLength(1); j++)
+            Array[i,j] = new Random().Next(1,21);
+}
+
+void PrintArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+            System.Console.Write($"{Array[i,j],3} ");
+        System.Console.WriteLine();
+    }
+}
+
+System.Console.WriteLine("Необходимо указать прямоугольный двумерный массив (кол. строк не равно кол. столбцов)");
+System.Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(System.Console.ReadLine());
+
+if(rows == columns) { System.Console.WriteLine("Нужно ввести прямоугольный массив!"); return; }
+int[,] Array = new int[rows,columns];
+
+RandomFillArray(Array);
+PrintArray(Array);
+FindLargestSumOfElements(Array);
