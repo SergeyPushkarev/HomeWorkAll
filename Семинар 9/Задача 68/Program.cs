118b2cc54fb92cb4e1e78f692f852216348e5aa4
Sergey <samtriezo@gmail.com>
2023-06-24 11:17:30 +0700

    сделал задачу 68 семинара 9

--- Семинар 9/Задача 68/Program.cs
+++ Семинар 9/Задача 68/Program.cs
@@ -2,3 +2,20 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
+int CalculateAckermanFunction (int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if((m > 0) && (n == 0))
+        return CalculateAckermanFunction(m - 1, 1);
+    else if((m > 0) && (n > 0))
+        return CalculateAckermanFunction(m - 1, CalculateAckermanFunction(m, n - 1));
+    else return n + 1;
+}
+
+System.Console.Write("Введите первое неотрицательное число (m): ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе неотрицательное число (n): ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Результат функции аккермана для A({m},{n}) равен: {CalculateAckermanFunction(m, n)}");
