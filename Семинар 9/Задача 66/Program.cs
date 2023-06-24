e302ae426fbb2807440782d7a66939193ce30134
Sergey <samtriezo@gmail.com>
2023-06-24 10:43:13 +0700

    сделал задачу 66 семинара 9

--- Семинар 9/Задача 66/Program.cs
+++ Семинар 9/Задача 66/Program.cs
@@ -2,3 +2,15 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+int ReturnSumInBetween (int num1, int num2, int sum=0)
+{
+    if(num1 == num2) return num1;
+    return num1 + ReturnSumInBetween(num1+1,num2);
+}
+
+System.Console.Write("Введите первое целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Сумма всех чисел от {num1} до {num2} равна: {ReturnSumInBetween(num1, num2)}");
